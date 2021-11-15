b3e36c68db987043d1a8987225b177979f633e79
Henry Rocha <henryrocha@protonmail.com>
2021-11-15 20:10:20 -0300

    feat: working pyramid and triangle

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 #include "rtweekend.hpp"
 #include "camera.hpp"
 #include "material.hpp"
+#include "pyramid.hpp"
+#include "triangle.hpp"
 
 // Função auxiliar para criar um fundo de imagem colorido.
 color ray_color(const ray& r, const hittable& world, int depth) {
@@ -35,25 +37,42 @@ int main()
 {
     // Image settings.
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 480;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
     
     // World settings and init.
-    auto R = cos(pi/4);
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    auto mat_red = make_shared<lambertian>(color(1.0, 0.0, 0.0));
+    auto mat_green = make_shared<lambertian>(color(0.0, 1.0, 0.0));
+    auto mat_blue = make_shared<lambertian>(color(0.0, 0.0, 1.0));
+    auto mat_metal  = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    // world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    // world.add(make_shared<sphere>(point3(0.0, 0.0, 0.0), 0.5, mat_blue));
+    // world.add(make_shared<sphere>(point3(-1.0, 0.0, 0.0), 0.5, mat_red));
+    // world.add(make_shared<sphere>(point3( -1.0,    0.0, -1.0),  0.5, mat_metal));
+
+    world.add(make_shared<pyramid>(
+        point3(0.0, 0, 0.0), 1.0, 0.5,
+        mat_green,
+        mat_red,
+        mat_blue,
+        mat_red,
+        mat_blue
+    ));
+
+    world.add(make_shared<pyramid>(
+        point3(1.0, 0, 0.0), 1.0, 0.5,
+        mat_green,
+        mat_red,
+        mat_blue,
+        mat_red,
+        mat_blue
+    ));
 
     // Camera settings.
     const auto viewport_height = 2.0;
@@ -61,9 +80,9 @@ int main()
     const auto focal_length = 1.0;
 
     // Camera position.
-    point3 lookfrom(3,3,2);
-    point3 lookat(0,0,-1);
-    vec3 vup(0,1,0);
+    point3 lookfrom(0, 2, 5);
+    point3 lookat(0, 0, 0);
+    vec3 vup(0, 1, 0);
     auto dist_to_focus = (lookfrom-lookat).length();
     auto aperture = 2.0;
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
