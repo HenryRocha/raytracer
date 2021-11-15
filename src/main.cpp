da3e77178e453559f50e6355d906514707763f30
Henry Rocha <henryrocha@protonmail.com>
2021-11-15 17:22:44 -0300

    feat: defocus blur

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,13 @@ int main()
     const auto focal_length = 1.0;
 
     // Camera position.
-    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
+    point3 lookfrom(3,3,2);
+    point3 lookat(0,0,-1);
+    vec3 vup(0,1,0);
+    auto dist_to_focus = (lookfrom-lookat).length();
+    auto aperture = 2.0;
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+
     const auto origin = point3(0, 0, 0);
     auto horizontal = vec3(viewport_width, 0, 0);
     auto vertical = vec3(0, viewport_height, 0);
